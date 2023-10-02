6814823cb6f35c1dbdad6db0299914a7bb92602d
Lety_wood <hola@leticiawood.me>
2023-10-01 21:53:02 -0600

    Clase 03

---
+++ src/Components/List/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Box,Btn } from "../UI";
+import { lista } from "../../info";
+
+const List = () => {
+    console.log(lista)
+    return <Box>
+                {
+                    lista.cargos.map( ({id, type, value, from, date}) => {
+                       return <div key={id}>
+                            <span>
+                                {type}
+                            </span>
+                            <span>
+                                {from}
+                            </span>
+                            <span>
+                                {value}
+                            </span>
+                        </div>
+                    })
+                }
+                <Btn>Ver más</Btn>
+            </Box>
+}
+
+export default List
